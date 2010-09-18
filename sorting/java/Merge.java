12d5a45c00c544664617283df0a6c0655c98c589
hemanth <hemanth.hm@gmail.com>
2010-09-18 14:07:18 +0530

    Added Merge sort

--- sorting/java/Merge.java
+++ sorting/java/Merge.java
@@ -1,92 +1,32 @@
-public static void mergesort(int[ ] set, int first, int n)
-{
-
- int n1; // Size of the first half of the array
-
- int n2; // Size of the second half of the array
-
-
-
- if (n > 1)
-
- {
-
-    // Compute sizes of the two halves
-
-    n1 = n / 2;
-
-    n2 = n - n1;
-
-
-
-    mergesort(set, first, n1); // Sort set[first] through set[first+n1-1]
-
-    mergesort(set, first + n1, n2); // Sort set[first+n1] to the end
-
-
-
-    // Merge the two sorted halves.
-
-    merge(set, first, n1, n2);
-
- }
-
-   } 
-
-  
-
-private static void merge(int[ ] set, int first, int n1, int n2)
-{
-
-	int[ ] temp = new int[n1+n2]; 
-
-	int toTemp  = 0; 
-
-	int firstHalf = 0; 
-
-	int secondHalf = 0; 
-
-	int i;
-
-
- // Merge elements
- while ((firstHalf < n1) && (secondHalf < n2))
- {
-
-    if (set[first + firstHalf] < set[first + n1 + secondHalf])
-
-  temp[toTemp++] = set[first + (firstHalf++)];
-
-    else
-
-  temp[toTemp++] = set[first + n1 + (secondHalf++)];
-
- }
-
-
-
- // cp (left,right) subarrays.
-
- while (firstHalf < n1)
-
-    temp[toTemp++] = set[first + (firstHalf++)];
-
- while (secondHalf < n2)
-
-    temp[toTemp++] = set[first + n1 + (secondHalf++)];
-
-
-
- // temp back to the set array.
-
- for (i = 0; i < n1+n2; i++)
-
-    set[first + i] = temp[i];
-
-   }
-
-   
-
+import java.util.LinkedList;
+public class Merge<E extends Comparable<? super E>> {
+	public LinkedList<E> mergeSort(LinkedList<E> m){
+		if(m.size() <= 1) return m;
+ 
+		int middle= m.size() / 2;
+		LinkedList<E> left= new LinkedList<E>();
+		for(int i= 0;i < middle;i++) left.add(m.get(i));
+		LinkedList<E> right= new LinkedList<E>();
+		for(int i= middle;i < m.size();i++) right.add(m.get(i));
+ 
+		right= mergeSort(right);
+		left= mergeSort(left);
+		LinkedList<E> result= merge(left, right);
+ 
+		return result;
+	}
+ 
+	public LinkedList<E> merge(LinkedList<E> left, LinkedList<E> right){
+		LinkedList<E> result= new LinkedList<E>();
+ 
+		while(!left.isEmpty() && !right.isEmpty()){
+			//change the direction of this comparison to change the direction of the sort
+			if(left.peek().compareTo(right.peek()) <= 0) result.add(left.remove());
+			else result.add(right.remove());
+		}
+ 
+		result.addAll(left);
+		result.addAll(right);
+		return result;
+	}
 }
-
-
