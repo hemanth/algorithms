4003f2b7825163aa5a4947048b36b7ce06bc3271
hemanth <hemanth.hm@gmail.com>
2010-09-18 14:15:14 +0530

    Added heap sort py code

---
+++ sorting/heap.py
@@ -0,0 +1,22 @@
+def heapsort(lst):
+ 
+  for start in range((len(lst)-2)/2, -1, -1):
+    siftdown(lst, start, len(lst)-1)
+ 
+  for end in range(len(lst)-1, 0, -1):
+    lst[end], lst[0] = lst[0], lst[end]
+    siftdown(lst, 0, end - 1)
+  return lst
+ 
+def siftdown(lst, start, end):
+  root = start
+  while True:
+    child = root * 2 + 1
+    if child > end: break
+    if child + 1 <= end and lst[child] < lst[child + 1]:
+      child += 1
+    if lst[root] < lst[child]:
+      lst[root], lst[child] = lst[child], lst[root]
+      root = child
+    else:
+      break
