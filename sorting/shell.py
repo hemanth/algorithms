d6899ce2c5a3fa015921a06d8aeeb0bffd1f315f
hemanth <hemanth.hm@gmail.com>
2010-09-18 14:23:44 +0530

    Inline shell short py code added

---
+++ sorting/shell.py
@@ -0,0 +1,9 @@
+def shell(seq):
+    inc = len(seq) // 2
+    while inc:
+        for i, el in enumerate(seq):
+            while i >= inc and seq[i - inc] > el:
+                seq[i] = seq[i - inc]
+                i -= inc
+            seq[i] = el
+        inc = 1 if inc == 2 else int(inc * 5.0 / 11)
