ea158744b46aacc22304e096c6f4abdfd711c727
hemanth <hemanth.hm@gmail.com>
2010-09-19 17:25:56 +0530

    Made Bubble sort more better, by adding a class, rather than just the definition.

--- sorting/java/Bubble.java
+++ sorting/java/Bubble.java
@@ -1,14 +1,36 @@
-public static <E extends Comparable<? super E>> void bubbleSort(E[] comparable) {
-    boolean changed = false;
-    do {
-        changed = false;
-        for (int a = 0; a < comparable.length - 1; a++) {
-            if (comparable[a].compareTo(comparable[a + 1]) > 0) {
-                E tmp = comparable[a];
-                comparable[a] = comparable[a + 1];
-                comparable[a + 1] = tmp;
-                changed = true;
-            }
-        }
-    } while (changed);
+/**
+ * @author hemanth.hm
+ * Site : www.h3manth.com
+ *
+ */
+public class BubbleSort {
+
+	/**
+	 * @param args
+	 */
+	public static <E extends Comparable<? super E>> void bubbleSort(E[] comparable) {
+	    boolean changed = false;
+	    do {
+	        changed = false;
+	        for (int a = 0; a < comparable.length - 1; a++) {
+	            if (comparable[a].compareTo(comparable[a + 1]) > 0) {
+	                E tmp = comparable[a];
+	                comparable[a] = comparable[a + 1];
+	                comparable[a + 1] = tmp;
+	                changed = true;
+	            }
+	        }
+	    } while (changed);
+	}
+
+	@SuppressWarnings("unchecked")
+	public static void main(String[] args) {
+	   Comparable tosort [] = {0,1,-2,-4,6,8,9,99};
+		
+		BubbleSort.bubbleSort(tosort);
+		
+		 for (Comparable sorted : tosort) {
+	         System.out.println(sorted);
+	       }
+	}
 }
