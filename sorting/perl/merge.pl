6fec47f6f30dd541b3717e26fa5e50af8357ee80
hemanth <hemanth.hm@gmail.com>
2010-09-28 12:41:58 +0530

    Major fix on merge, wrong file was pushed\!

--- sorting/perl/merge.pl
+++ sorting/perl/merge.pl
@@ -1,14 +1,24 @@
 #!/usr/bin/perl
-
-sub insertion_sort {
-  $to_sort = shift;
-  for ($i = 0; $i <= $#{$to_sort}; $i++) {
-    $j = $i - 1;
-    $key = $to_sort->[$i];
-    while ($to_sort->[$j] > $key && $j >= 0) {
-      $to_sort->[$j + 1] = $arr->[$j];
-        $j -= 1;
-      }
-    $to_sort->[$j + 1] = $key;
-  }
+sub mergesort
+{
+        my @i = @_;
+        return @i unless scalar @i > 1;
+ 
+        my $m = int $#i / 2;
+        my @left = &mergesort(@i[0..$m]);
+        my @right = &mergesort(@i[$m+1..$#i]);
+ 
+        return &merge(\@left,\@right);
+}
+ 
+sub merge
+{
+        my($left,$right) = @_;
+        my @r = ();
+ 
+        push @r, shift @{(@$left[0] < @$right[0]?$left:$right)} while(@$left and @$right);
+        push @r, shift @$left while(@$left);
+        push @r, shift @$right while(@$right);
+ 
+        return @r;
 }
