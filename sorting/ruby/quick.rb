005d3617c0431c6be653c97c6cf86a643beb1344
hemanth <hemanth.hm@gmail.com>
2010-09-28 12:25:09 +0530

    Quickly added quick sort

---
+++ sorting/ruby/quick.rb
@@ -0,0 +1,11 @@
+#/usr/bin/ruby
+# Works with array
+class Array
+  def quick_sort
+    return self if length <= 1
+    pivot = self[length / 2]
+    find_all { |i| i <  pivot }.quick_sort +
+      find_all { |i| i == pivot } +
+      find_all { |i| i >  pivot }.quick_sort
+  end
+end
