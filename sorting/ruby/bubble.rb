99148af70c95787b7558752e8efd91530afe8dfc
hemanth <hemanth.hm@gmail.com>
2010-09-28 12:15:48 +0530

    Made bubble more generic

--- sorting/ruby/bubble.rb
+++ sorting/ruby/bubble.rb
@@ -1,14 +1,14 @@
 #!/usr/bin/ruby
 # Works on a array 
 # tosort.bubblesort!
-
-def bubblesort!
-    length.times do |j|
-      for i in 1...(length - j)
-        if self[i] < self[i - 1]
-          self[i], self[i - 1] = self[i - 1], self[i]
-        end
-      end
-    end
+class Array
+	def bubblesort!
+		length.times do |j|
+			for i in 1...(length - j)
+				if self[i] < self[i - 1]
+				self[i], self[i - 1] = self[i - 1], self[i]
+				end
+			end
+		end
 end
 
